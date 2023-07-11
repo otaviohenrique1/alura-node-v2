33d391138c14751cab4109924dbcc2ec66a0fe3f
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-07-11 20:26:52 -0300

    commit 22

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -6,14 +6,18 @@ import RequisicaoIncorreta from "../errors/RequisicaoIncorreta.js";
 class LivroController {
   static listarLivros = async (req, res, next) => {
     try {
-      let { limite = 5, pagina = 1 } = req.query;
+      let { limite = 5, pagina = 1, ordenacao = "_id:-1" } = req.query;
+
+      let [campoOrdenacao, ordem] = ordenacao.split(":");
 
       limite = parseInt(limite);
       pagina = parseInt(pagina);
+      ordem = parseInt(ordem);
 
       if (limite > 0 && pagina > 0) {
         const livrosLista = await livros
           .find()
+          .sort({ [campoOrdenacao]: ordem }) // -1 => Decrescente / 1 => Crescente
           .skip((pagina - 1) * limite) // Quantos livros vao ser pulados
           .limit(limite) // Quantos livros vao ser exibidos
           .populate("autor")
@@ -28,6 +32,33 @@ class LivroController {
       // res.status(500).json({ erro: "Ocorreu um erro ao buscar os livros." });
     }
   };
+
+  // static listarLivros = async (req, res, next) => {
+  //   try {
+  //     let { limite = 5, pagina = 1, campoOrdenacao = "_id", ordem = -1 } = req.query;
+
+  //     limite = parseInt(limite);
+  //     pagina = parseInt(pagina);
+  //     ordem = parseInt(ordem);
+
+  //     if (limite > 0 && pagina > 0) {
+  //       const livrosLista = await livros
+  //         .find()
+  //         .sort({ [campoOrdenacao]: ordem }) // -1 => Decrescente / 1 => Crescente
+  //         .skip((pagina - 1) * limite) // Quantos livros vao ser pulados
+  //         .limit(limite) // Quantos livros vao ser exibidos
+  //         .populate("autor")
+  //         .exec();
+  //       res.status(200).json(livrosLista);
+  //     } else {
+  //       next(new RequisicaoIncorreta());
+  //     }
+  //   } catch (error) {
+  //     next(error);
+  //     // console.error(error);
+  //     // res.status(500).json({ erro: "Ocorreu um erro ao buscar os livros." });
+  //   }
+  // };
 
   // static listarLivrosTeste = async (req, res, next) => {
   //   try {

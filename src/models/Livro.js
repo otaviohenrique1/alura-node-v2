d108533feac94e7e5c35d05bc24a6b4c30fc2275
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-07-11 09:48:01 -0300

    commit 10

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -5,18 +5,32 @@ const livroSchema = new mongoose.Schema(
     id: { type: String },
     titulo: {
       type: String,
-      required: [true, "O título do livro é obrigatório"]
+      required: [true, "O título do livro é obrigatório"],
     },
     autor: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "autores",
-      required: [true, "O(a) autor(a) é obrigatório"]
+      required: [true, "O(a) autor(a) é obrigatório"],
     },
     editora: {
       type: String,
-      required: [true, "A editora é obrigatório"]
+      required: [true, "A editora é obrigatório"],
+      // enum: {
+      //   values: ["Casa do código", "Alura"],
+      //   message: "A editora {VALUE} fornecida não é um valor permitido."
+      // }
     },
-    numeroPaginas: { type: Number },
+    numeroPaginas: {
+      type: Number,
+      // min: [10, "O número de paginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"],
+      // max: [5000, "O número de paginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"],
+      validate: {
+        validator: (valor) => {
+          return valor >= 10 && valor <= 5000;
+        },
+        message: "O número de paginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}",
+      }
+    },
   }
 );
 

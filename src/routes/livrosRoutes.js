f6d37c9f0c3294bd6339c97496ddeacf3bbc9368
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-07-12 10:30:56 -0300

    commit 25

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router
   .get("/livros", LivroController.listarLivros, paginar)
-  .get("/livros/busca", LivroController.listarLivroPorFiltro, paginar)
+  .get("/livros/busca", LivroController.listarLivroPorFiltro)
   // .get("/livros/busca", LivroController.listarLivroPorEditora)
   .get("/livros/:id", LivroController.listarLivroPorId)
   .post("/livros", LivroController.cadastrarLivro)
